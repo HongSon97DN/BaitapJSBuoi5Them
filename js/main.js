50712fd14dd87881db960b22df40c540aae690b4
Son <alvinichi01@gmail.com>
2022-07-22 11:38:50 +0700

    Bài tập JS buổi 5 thêm

--- js/main.js
+++ js/main.js
@@ -41,3 +41,51 @@ function tinhTienThue() {
     }
 }
 document.getElementById("tinhTienThue").onclick = tinhTienThue;
+
+// Bài 2
+
+var hoaDonND = 4.5;
+var coBanND = 20.5;
+var caoCapND = 7.5;
+
+var hoaDonDN = 15;
+var coBanDN_0_10 = 75;
+var caoCapDN = 50;
+
+function showOnOff() {
+   var showUpDown = document.getElementById("soKetNoi");
+   var value = Number(document.getElementById("loaiKH").value);
+   
+   if (value == 0 || value == 4.5) {
+        showUpDown.innerHTML = showUpDown.classList.add("showOff");
+       
+   } else if (value == 15) {
+        showUpDown.innerHTML = showUpDown.classList.remove("showOff");       
+   }
+   
+}
+
+function tinhTienCap() {
+    var hoaDonCoBan = Number(document.getElementById("loaiKH").value);
+    var tenKH = document.getElementById("maKH").value;
+    var soKenhCC = Number(document.getElementById("kenhCaoCap").value);
+
+    if (hoaDonCoBan == 4.5) {      
+        var tongTienCap = hoaDonND + coBanND + (soKenhCC * caoCapND);
+
+        document.getElementById("txtResult1").innerHTML = "Mã KH: " + tenKH + " Tiền cáp: $ " + tongTienCap;
+    } else if (hoaDonCoBan = 15) {
+        var soKenhKT = Number(document.getElementById("soKetNoi").value);
+
+        if (soKenhKT <= 10) {
+            var tongTienCap = hoaDonDN + coBanDN_0_10 + (soKenhCC * caoCapDN);
+
+            document.getElementById("txtResult1").innerHTML = "Mã KH: " + tenKH + " Tiền cáp: $ " + tongTienCap;
+        } else {
+            var tongTienCap = hoaDonDN + coBanDN_0_10 + ((soKenhKT - 10) * 5) + (soKenhCC * caoCapDN);
+
+            document.getElementById("txtResult1").innerHTML = "Mã KH: " + tenKH + " Tiền cáp: $ " + tongTienCap;
+        }
+    }
+}
+document.getElementById("tinhTienCap").onclick = tinhTienCap;
